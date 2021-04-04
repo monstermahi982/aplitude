e7eb6b1e9533d62207bff56efc52dfa192297322
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2021-04-04 18:45:36 +0530

    third problem

---
+++ src/array1.java
@@ -0,0 +1,34 @@
+public class array1 {
+    public static void main(String[] args) {
+//        simple array problem
+        int [] array = {2,3,5,1,3};
+
+        System.out.println("---------------------------------");
+
+//        length of array
+        System.out.println(array.length);
+
+        System.out.println("---------------------------------");
+
+//        foreach loop
+        for (int x: array) {
+            System.out.println(x);
+        }
+
+        System.out.println("---------------------------------");
+
+//        simple for loop
+        for(int i = 0 ; i<array.length;i++){
+            System.out.println(array[i]);
+        }
+
+        System.out.println("---------------------------------");
+
+//        reverse a array
+        for (int i=array.length - 1 ; i>=0 ; i--){
+            System.out.println(array[i]);
+        }
+
+
+    }
+}
---
+++ src/problem1.java
@@ -0,0 +1,42 @@
+//        Check if a key is present in every segment of size k in an array
+
+//        Given an array arr[] and size of array is n and one another key x, and give you a segment size k.
+//        The task is to find that the key x present in every segment of size k in arr[].
+
+//        Input :
+//        arr[] = { 3, 5, 2, 4, 9, 3, 1, 7, 3, 11, 12, 3}
+//        x = 3
+//        k = 3
+//        Output : Yes
+//        There are 4 non-overlapping segments of size k in the array, {3, 5, 2}, {4, 9, 3}, {1, 7, 3} and {11, 12, 3}. 3 is present all segments.
+//        Input :
+//        arr[] = { 21, 23, 56, 65, 34, 54, 76, 32, 23, 45, 21, 23, 25}
+//        x = 23
+//        k = 5
+//        Output :Yes
+//        There are three segments and last segment is not full {21, 23, 56, 65, 34}, {54, 76, 32, 23, 45} and {21, 23, 25}.
+//        23 is present all window.
+//        Input :arr[] = { 5, 8, 7, 12, 14, 3, 9}
+//        x = 8
+//        k = 2
+//        Output : No
+
+
+import java.util.*;
+public class problem1 {
+
+//    static boolean findSize(int N , int [] arr , int x , int k){
+//        boolean b = false;
+////        for (int i = 0; i < N ; i = i + k ){
+////
+////        }
+//    }
+
+    public static void main(String[] args) {
+
+        int arr [] = {3, 5, 2, 4,  9,  3, 1, 7, 3, 11, 12, 3};
+        int x = 3 , k = 3;
+        int n = arr.length;
+
+    }
+}
