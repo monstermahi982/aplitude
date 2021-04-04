13f64b0b6a7bcf339407eac7c79e0c617c692dda
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2021-04-04 18:57:11 +0530

    find small and big value

--- src/problem2.java
+++ src/problem2.java
@@ -1,5 +1,29 @@
 public class problem2 {
+     static int smaallfun(int n , int [] array){
+        int a = array[0];
+        for (int i = 1 ; i < n ; i++){
+            if(array[i] < a){
+                a = array[i];
+            }
+         }
+    return a;
+     }
+     static int bigfun(int n , int [] array){
+         int a = array[0];
+         for (int i = 1 ; i < n ; i++){
+             if(array[i] > a){
+                 a = array[i];
+             }
+         }
+         return a;
+     }
+
     public static void main(String[] args) {
-        System.out.println("hello monster");
+        int array [] = {200 , 43 , 235 , 10 , 5 , 2  , 400 };
+        int n = array.length;
+        int small_value = smaallfun(n , array);
+        int big_value = bigfun(n , array);
+        System.out.println(small_value);
+        System.out.println(big_value);
     }
 }
