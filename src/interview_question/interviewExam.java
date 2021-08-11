34fa3005e00302305051775b47502423b6145821
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2021-08-11 20:32:04 +0530

    11aug21 2

--- src/interview_question/interviewExam.java
+++ src/interview_question/interviewExam.java
@@ -1,8 +1,19 @@
 package interview_question;
 
+class animal{
+    String name = "cow-animal";
+    void action(){
+        System.out.println(name + " is walking");
+    }
+}
+
+class cow extends animal{
+
+}
 
 public class interviewExam {
     public static void main(String[] args) {
-
+        cow c = new cow();
+        System.out.println(c.name);
     }
 }
