0facb433aaf662da5954d45f2f305cd5fdfa0186
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2021-08-12 21:06:41 +0530

    12aug21 2

--- src/company_question/question4.java
+++ src/company_question/question4.java
@@ -1,7 +1,63 @@
 package company_question;
 
+class BinarySearchTree{
+    class Node{
+        int data;
+        Node left , right;
+        Node(int data){
+            this.data= data;
+            left = right = null;
+        }
+    }
+    Node root;
+
+    BinarySearchTree(){
+        root = null;
+    }
+
+    void insert(int value){
+        root = insertNode(root , value);
+    }
+
+     Node insertNode(Node root, int value) {
+        if(root == null){
+            root = new Node(value);
+            return root;
+        }
+        if(root.data > value){
+            root.left = insertNode(root.left,value);
+        }else if(root.data < value){
+            root.right = insertNode(root.right , value);
+        }
+
+        return root;
+    }
+
+
+    void inorder(){
+        inorderNode(root);
+    }
+
+     void inorderNode(Node root) {
+        if(root != null){
+            inorderNode(root.left);
+            System.out.println(root.data);
+            inorderNode(root.right);
+        }
+    }
+
+}
+
 public class question4 {
     public static void main(String[] args) {
-
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(70);
+        tree.insert(60);
+        tree.insert(80);
+        tree.inorder();
     }
 }
