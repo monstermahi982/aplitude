c78d50776ef140fbd3879f99aef82b1b49cdf026
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2021-09-20 19:19:02 +0530

    19sep21

--- src/company_question/problem21.java
+++ src/company_question/problem21.java
@@ -1,10 +1,19 @@
 package company_question;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class problem21 {
     public static void main(String[] args) {
         int array [] = {0,1,2,3,4,5};
         int count = 0;
         isDone(array, count);
+        Set s = new HashSet();
+        ArrayList s2 = new ArrayList();
+
+        Scanner s1 =  new Scanner(System.in);
     }
 
     private static void isDone(int[] array, int count) {
