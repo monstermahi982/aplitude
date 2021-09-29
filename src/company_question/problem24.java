50484a7e5ba270de7381acac7535da6a5f09cf2d
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2021-09-29 13:09:13 +0530

    28sep21

--- src/company_question/problem24.java
+++ src/company_question/problem24.java
@@ -1,4 +1,34 @@
 package company_question;
 
+import java.util.Scanner;
+
 public class problem24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int array [] = new int[N];
+        int k = sc.nextInt();
+        for (int i=0;i<array.length;i++){
+            array[i] = sc.nextInt();
+        }
+        int ans = findAns(array,k);
+        System.out.println(ans);
+    }
+
+    private static int findAns(int[] array, int k) {
+        int even = Integer.MAX_VALUE;
+        int odd = Integer.MAX_VALUE;
+        for(int i=0;i<k;i++){
+            if(i % 2 == 0){
+                if(array[i] < even){
+                    even = array[i];
+                }
+            }else {
+                if(array[i] < odd){
+                    odd = array[i];
+                }
+            }
+        }
+        return Math.max(odd,even);
+    }
 }
--- src/company_question/problem25.java
+++ src/company_question/problem25.java
@@ -1,4 +1,14 @@
 package company_question;
 
+import java.util.ArrayList;
+
 public class problem25 {
+    public static void main(String[] args) {
+        int n = 34512678;
+        ArrayList array = new ArrayList();
+        System.out.println(n % 10);
+//        while (n > 0){
+//            array
+//        }
+    }
 }
